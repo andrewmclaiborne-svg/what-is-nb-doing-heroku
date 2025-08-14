fd339ab43b9ade35768a7ece57596bb97e09ef15
andrewmclaiborne-svg <andrew.m.claiborne@gmail.com>
2025-08-13 18:44:52 -1000

    Update app.py

--- app.py
+++ app.py
@@ -83,4 +83,4 @@ def wind_wave_data():
 if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=8080, debug=True)
+    app.run(host="0.0.0.0", port=port)
